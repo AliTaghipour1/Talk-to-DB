11f074b4144b456c9a9d4a1b122e0d34f8230b9a
alitaghipour <alitaghipour80@gmail.com>
2025-12-05 15:18:33 +0330

    Adds config handler

---
+++ internal/config/config.go
@@ -0,0 +1,43 @@
+package config
+
+import (
+	"encoding/json"
+	"fmt"
+	"os"
+)
+
+type TalkToDBConfig struct {
+	DebugMode bool
+	CliBot    Bot
+	Databases []Database
+}
+type Driver string
+
+const (
+	Postgres Driver = "postgres"
+	MySQL    Driver = "mysql"
+)
+
+type Database struct {
+	Host   string
+	Port   string
+	User   string
+	Pass   string
+	Name   string
+	Driver Driver
+}
+
+type Bot struct {
+	Token string
+}
+
+func NewTalkToDbConfig() *TalkToDBConfig {
+	config := os.Getenv("config")
+	var result TalkToDBConfig
+	err := json.Unmarshal([]byte(config), &result)
+	if err != nil {
+		panic(fmt.Errorf("failed to read config: %w", err))
+	}
+
+	return &result
+}
